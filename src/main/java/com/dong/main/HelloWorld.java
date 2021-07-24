b8719a29e8e9b585650191df3111fd470230f295
zhangdong <zhangdong@ZD.local>
2021-07-24 11:22:44 +0800

    second commit

--- src/main/java/com/dong/main/HelloWorld.java
+++ src/main/java/com/dong/main/HelloWorld.java
@@ -3,5 +3,6 @@ package com.dong.main;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("1111111");
+        System.out.println("2222222");
     }
 }
