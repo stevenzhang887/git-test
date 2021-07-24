f839c786e00b7a82d7f6911f0779e102201600d5
zhangdong <zhangdong@ZD.local>
2021-07-24 11:23:53 +0800

    hot-fix first commit

--- src/main/java/com/dong/main/HelloWorld.java
+++ src/main/java/com/dong/main/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("1111111");
         System.out.println("2222222");
+        System.out.println("3333333--hot-fix");
     }
 }
