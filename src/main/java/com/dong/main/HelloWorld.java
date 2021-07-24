7faa7429f2fe379613e7a510ed3e617065faad2c
stevenzhang887 <58079087+stevenzhang887@users.noreply.github.com>
2021-07-24 11:46:09 +0800

    github-edit

--- src/main/java/com/dong/main/HelloWorld.java
+++ src/main/java/com/dong/main/HelloWorld.java
@@ -8,5 +8,6 @@ public class HelloWorld {
         System.out.println("4444444");
         System.out.println("4444444--hot-fix");
         System.out.println("5555555-master");
+        System.out.println("6666666-github-edit");
     }
 }
