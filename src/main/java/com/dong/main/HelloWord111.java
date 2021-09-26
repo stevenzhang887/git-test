ead757ea32ff60a30afb5646263f2332394c19e4
zhangdong <zhangdong@ZD.local>
2021-09-26 21:53:39 +0800

    HelloWord111 committed

--- src/main/java/com/dong/main/HelloWord111.java
+++ src/main/java/com/dong/main/HelloWord111.java
@@ -2,13 +2,22 @@ package com.dong.main;
 
 public class HelloWord111 {
     public static void main(String[] args) {
+        System.out.println("this is a 2021-09-06 9999");
+        System.out.println("this is a 2021-09-06 8888");
+        System.out.println("this is a 2021-09-06 7777");
+
         System.out.println("this is a 2021-09-06");
         System.out.println("this is a 2021-09-06 21:26");
         System.out.println("this is a 2021-09-06");
-        System.out.println("this is a 2021-09-06 21:26"); 
+        System.out.println("this is a 2021-09-06 21:26");
         System.out.println("this is a 2021-09-06");
-        System.out.println("this is a 2021-09-06 21:26"); 
+        System.out.println("this is a 2021-09-06 21:26");
         System.out.println("this is a 2021-09-06");
-        System.out.println("this is a 2021-09-06 21:26"); 
+        System.out.println("this is a 2021-09-06 21:26");
+
+        System.out.println("this is a 2021-09-06 9999");
+        System.out.println("this is a 2021-09-06 8888");
+        System.out.println("this is a 2021-09-06 7777");
+
     }
 }
