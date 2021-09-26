84671422cf70b15b7eab5fecba296ecc837093e0
stevenzhang887 <58079087+stevenzhang887@users.noreply.github.com>
2021-09-26 21:30:11 +0800

    添加三行

--- src/main/java/com/dong/main/HelloWord111.java
+++ src/main/java/com/dong/main/HelloWord111.java
@@ -3,6 +3,12 @@ package com.dong.main;
 public class HelloWord111 {
     public static void main(String[] args) {
         System.out.println("this is a 2021-09-06");
-        System.out.println("this is a 2021-09-06 21:26");        
+        System.out.println("this is a 2021-09-06 21:26");
+        System.out.println("this is a 2021-09-06");
+        System.out.println("this is a 2021-09-06 21:26"); 
+        System.out.println("this is a 2021-09-06");
+        System.out.println("this is a 2021-09-06 21:26"); 
+        System.out.println("this is a 2021-09-06");
+        System.out.println("this is a 2021-09-06 21:26"); 
     }
 }
