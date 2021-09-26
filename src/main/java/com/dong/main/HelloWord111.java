3cbbda0aef582a8186bb550e84bd7e6363748672
stevenzhang887 <58079087+stevenzhang887@users.noreply.github.com>
2021-09-26 21:22:39 +0800

    Update HelloWord111.java
    添加2行

--- src/main/java/com/dong/main/HelloWord111.java
+++ src/main/java/com/dong/main/HelloWord111.java
@@ -3,5 +3,7 @@ package com.dong.main;
 public class HelloWord111 {
     public static void main(String[] args) {
         System.out.println("this is a 2021-09-06");
+        System.out.println("this is a 2021-09-06");
+        System.out.println("this is a 2021-09-06");
     }
 }
