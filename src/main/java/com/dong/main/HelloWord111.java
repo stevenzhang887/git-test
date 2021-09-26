d970606ac0a30968a7d3a50b47051bf04f635c05
stevenzhang887 <58079087+stevenzhang887@users.noreply.github.com>
2021-09-26 21:26:59 +0800

    删除三行

--- src/main/java/com/dong/main/HelloWord111.java
+++ src/main/java/com/dong/main/HelloWord111.java
@@ -3,10 +3,6 @@ package com.dong.main;
 public class HelloWord111 {
     public static void main(String[] args) {
         System.out.println("this is a 2021-09-06");
-        System.out.println("this is a 2021-09-06");
-        System.out.println("this is a 2021-09-06");
-        System.out.println("this is a 2021-09-06");
-        System.out.println("this is a 2021-09-06");
-        System.out.println("this is a 2021-09-06");
+        System.out.println("this is a 2021-09-06 21:26");        
     }
 }
